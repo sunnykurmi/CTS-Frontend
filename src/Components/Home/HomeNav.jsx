d74750632ebc201a6c033e2285815e7e4e938c57
sunnykurmi <sunnykurmiskks@gmail.com>
2024-10-26 18:59:13 +0530

    xxswdw

--- src/Components/Home/HomeNav.jsx
+++ src/Components/Home/HomeNav.jsx
@@ -2,103 +2,103 @@ import { RiCloseLine, RiMenuLine } from "@remixicon/react";
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import { asynccurrentUser } from "./../../store/Actions/userActions";
-import Headroom from "react-headroom";
+import { asynccurrentUser } from './../../store/Actions/userActions';
 
 const HomeNav = () => {
   const [menuOpen, setMenuOpen] = useState(false);
-  const dispatch = useDispatch();
-  const { isAuth } = useSelector((state) => state.user);
+  const dispatch = useDispatch()
+  const { isAuth } = useSelector((state) => state.user); 
 
+  
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
-
+  
   useEffect(() => {
     dispatch(asynccurrentUser());
   }, [dispatch]);
+  
 
   return (
     <>
-      <Headroom>
-        <div className="w-full h-fit py-5 flex items-center justify-center max-[600px]:h-16 bg-white z-[99999] max-[600px]:bg-[#FEF6EE]">
-          <div className="w-fit bg-[#fff0e1]  h-24 rounded-full flex items-center justify-evenly max-[600px]:w-full max-[600px]:rounded-none max-[600px]:pr-2 max-[600px]:h-fit max-[600px]:justify-between max-[600px]:px-5">
-            <Link className=" w-[15%] center" to="/">
-              <img
-                className="w-full max-[600px]:w-[30%]"
-                src="/Images/CTS   Logo.png"
-                alt=""
-              />
+      <div className="w-full h-40 flex items-center justify-center max-[600px]:h-16">
+        <div className="w-fit  bg-[#f8851127] h-24  rounded-full flex items-center justify-evenly max-[600px]:w-full max-[600px]:rounded-none max-[600px]:pr-2 max-[600px]:h-full max-[600px]:justify-between max-[600px]:px-5 ">
+          <img
+            className="w-[12%] max-[600px]:w-[30%]"
+            src="/Images/CTS   Logo.png"
+            alt=""
+          />
+          <div className="w-fit  gap-5 whitespace-nowrap h-full flex items-center justify-evenly font-bold text-[#393E46] max-[600px]:text-xs max-[600px]:w-fit max-[600px]:gap-2 max-[600px]:hidden">
+            <a className="hover-link" href="/">
+              HOME
+            </a>
+            <Link className="hover-link" to="/portfolio">
+              PORTFOLIO
             </Link>
-            <div className="w-[50vw] uppercase  whitespace-nowrap h-full flex items-center justify-center gap-8 font-bold text-[#393E46] max-[600px]:text-xs max-[600px]:w-fit max-[600px]:gap-2 max-[600px]:hidden">
-              <a className="hover-link" href="/">
-                HOME
-              </a>
-              <Link className="hover-link" to="/portfolio">
-                ivy acclerator
+            <Link className="hover-link" to="/apply-internship-form">
+              INTERNSHIP
+            </Link>
+            <Link className="hover-link" to="/services">
+              SERVICES
+            </Link>
+            <Link className="hover-link" to="/abroadstudy">
+              CTS ABROAD
+            </Link>
+            <a className="hover-link" href="#footer">
+              ABOUT
+            </a>
+          </div>
+          {isAuth ? (
+            <div className="w-fit  h-full flex items-center justify-evenly max-[600px]:hidden">
+              <Link to="/home">
+                <button className="w-32 h-14 rounded-full bg-[#F58612] text-white font-bold">
+                  DASHBOARD
+                </button>
               </Link>
-              <Link className="hover-link" to="/apply-internship-form">
-                exclusive
-              </Link>
-              <Link className="hover-link" to="/services">
-                community
-              </Link>
-              <Link className="hover-link" to="/services">
-                about us
-              </Link>
-             
             </div>
-            {isAuth ? (
-              <div className="w-fit h-full flex items-center justify-evenly max-[600px]:hidden">
-                <Link to="/home">
-                  <button className="w-32 h-14 rounded-full bg-[#F58612] text-white font-bold">
-                    DASHBOARD
-                  </button>
-                </Link>
-              </div>
-            ) : (
-              <div className="w-[25%] h-full flex items-center justify-evenly max-[600px]:hidden">
-                <Link to="/login" className="">
-                  <button className="w-32 h-14 rounded-full border-2 border-[#0000000c] font-bold">
-                    LOGIN
-                  </button>
-                </Link>
-                <Link to="/signup" className="max-[600px]:scale-50">
-                  <button className="w-32 h-14 rounded-full bg-[#F58612] text-white font-bold">
-                    REGISTER
-                  </button>
-                </Link>
-              </div>
-            )}
-            <div className="center flex-col min-[600px]:hidden">
-              <RiMenuLine onClick={toggleMenu} />
+          ) : (
+            <div className="w-[25%] h-full flex items-center justify-evenly max-[600px]:hidden">
+              <Link to="/login" className="">
+                <button className="w-32 h-14 rounded-full border-2 border-[#0000000c] font-bold">
+                  LOGIN
+                </button>
+              </Link>
+              <Link to="/signup" className="max-[600px]:scale-50">
+                <button className="w-32 h-14 rounded-full bg-[#F58612] text-white font-bold">
+                  REGISTER
+                </button>
+              </Link>
             </div>
+          )}
+          <div className="center flex-col min-[600px]:hidden">
+            <RiMenuLine onClick={toggleMenu} />
           </div>
         </div>
+      </div>
 
-        <div
-          className={`w-full h-fit pb-10 bg-white fixed top-0 left-0 z-[99] min-[600px]:hidden transition-transform duration-500 ${
-            menuOpen ? "translate-y-0" : "-translate-y-full"
-          }`}
-        >
-          <div className="w-full flex items-center justify-end p-5">
-            <RiCloseLine className="scale-150" onClick={toggleMenu} />
-          </div>
-          <div className="pr-5 flex-col uppercase text-2xl font-semibold center gap-3">
-            <a href="/">Home</a>
-            <Link to="/portfolio">Portfolio</Link>
-            <Link to="/apply-internship-form">Internship</Link>
-            <Link to="/abroadstudy">CTS Abroad</Link>
-            <Link to="/services">Services</Link>
-            <Link to="/login" className="">
-              Login
-            </Link>
-            <Link to="/signup" className="">
-              Register
-            </Link>
-          </div>
+      <div
+        className={`w-full h-fit pb-10 bg-white fixed top-0 left-0 z-[99] min-[600px]:hidden transition-transform duration-500 ${
+          menuOpen ? "translate-y-0" : "-translate-y-full"
+        }`}
+      >
+        {" "}
+        <div className="w-full flex items-center justify-end p-5">
+          <RiCloseLine className="scale-150" onClick={toggleMenu} />
         </div>
-      </Headroom>
+        <div className=" pr-5 flex-col uppercase text-2xl font-semibold center gap-3">
+          <a href="/">Home</a>
+          <Link to="/portfolio">Portfolio</Link>
+          <Link to="/apply-internship-form">Internship </Link>
+          <Link to="/abroadstudy">CTS Abroad</Link>
+          <Link to="/services">Services</Link>
+          <Link to="/login" className="">
+            Login{" "}
+          </Link>
+          <Link to="/signup" className="">
+            Register
+          </Link>
+        </div>
+      </div>
     </>
   );
 };
--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,8 +2,9 @@ import axios from "axios";
 
 const instance = axios.create({
 
-  baseURL: "http://localhost:3030",
+  // baseURL: "http://localhost:3030",
   // baseURL: "https://cts-roadmap-sunny-hwm6.onrender.com/",
+  baseURL:"https://cts-roadmap-sunny.onrender.com/",
   // baseURL: "https://cts-roadmap-copy.onrender.com/",
   // baseURL: "https://cts-roadmap.onrender.com",
   // baseURL: "https://cts-roadmap-two.onrender.com/",
