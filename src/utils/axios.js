272aa65cbce9256ec46be99ad5ec41363488a36b
sunnykurmi <sunnykurmiskks@gmail.com>
2024-10-28 13:24:18 +0530

    ss

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -4,11 +4,11 @@ const instance = axios.create({
 
   // baseURL: "http://localhost:3030",
   // baseURL: "https://cts-roadmap-sunny-hwm6.onrender.com/",
-  baseURL:"https://cts-roadmap-sunny.onrender.com/",
+  // baseURL:"https://cts-roadmap-sunny.onrender.com/",
   // baseURL: "https://cts-roadmap-copy.onrender.com/",
   // baseURL: "https://cts-roadmap.onrender.com",
   // baseURL: "https://cts-roadmap-two.onrender.com/",
-  // baseURL: "https://api.crosstheskylimits.online",
+  baseURL: "https://api.crosstheskylimits.online",
 
   
   
